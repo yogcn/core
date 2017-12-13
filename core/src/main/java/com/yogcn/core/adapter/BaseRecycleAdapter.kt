66a1099f4565f780a42c4b243c866132d580e630
lyndon <396815941@qq.com>
2017-12-13 18:28:42 +0800

    修改gradle配置

--- core/src/main/java/com/yogcn/core/adapter/BaseRecycleAdapter.kt
+++ core/src/main/java/com/yogcn/core/adapter/BaseRecycleAdapter.kt
@@ -8,24 +8,25 @@ import com.yogcn.core.base.RecyclerHolder
 /**
  * Created by lyndon on 2017/12/13.
  */
-abstract class BaseRecycleAdapter<T> : RecyclerView.Adapter<RecyclerHolder> {
-    protected var data: MutableCollection<T>
-    protected var context: Context
-    private var layoutRes: Int
-
-    constructor(recyclerView: RecyclerView, data: MutableCollection<T>, layoutRes: Int) {
-        this.context = recyclerView.context
-        this.data = data
-        this.layoutRes = layoutRes
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerHolder {
-
-    }
-
-    override fun onBindViewHolder(holder: RecyclerHolder?, position: Int) {
-
-    }
-
-    override fun getItemCount() = data.size
-}
+abstract class BaseRecycleAdapter<T>{}
+//    : RecyclerView.Adapter<RecyclerHolder> {
+//    protected var data: MutableCollection<T>
+//    protected var context: Context
+//    private var layoutRes: Int
+//
+//    constructor(recyclerView: RecyclerView, data: MutableCollection<T>, layoutRes: Int) {
+//        this.context = recyclerView.context
+//        this.data = data
+//        this.layoutRes = layoutRes
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerHolder {
+//
+//    }
+//
+//    override fun onBindViewHolder(holder: RecyclerHolder?, position: Int) {
+//
+//    }
+//
+//    override fun getItemCount() = data.size
+//}
